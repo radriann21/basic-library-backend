2bd93573467e79e18fc2c8c421c815ba511a9b6e
Adrian Rangel <adrianrangelargu@gmail.com>
2025-07-05 22:55:12 -0400

    feat: add Prestamo management with CRUD operations and routing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 // rutas
 import libroRoutes from "./routes/LibroRoutes.js";
 import usuarioRoutes from "./routes/UsuarioRoutes.js";
+import PrestamoRoutes from "./routes/PrestamoRoutes.js";
 
 const app = express()
 app.use(cors({
@@ -13,5 +14,6 @@ app.use(express.json());
 
 app.use("/api/libros", libroRoutes);
 app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/prestamos", PrestamoRoutes);
 
 export default app;
